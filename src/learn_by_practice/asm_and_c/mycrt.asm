1faf4e76d5bcf4cdbd41163e9882cd5ed77f332c
Your Name <you@example.com>
2021-09-13 09:13:47 +0000

    add comments

--- src/learn_by_practice/asm_and_c/mycrt.asm
+++ src/learn_by_practice/asm_and_c/mycrt.asm
@@ -1,8 +1,11 @@
 global mystart
 extern print
 
+; LD 需要一个 entry，默认为 _start，这里在命令行里面指定为 mystart
 mystart: 
 	call print
+; LD 需要一个 exit
+exit:
 	mov rax, 60
 	xor rdi, rdi
 	syscall
--- src/learn_by_practice/asm_and_c/mymain.c
+++ src/learn_by_practice/asm_and_c/mymain.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+// LD 里面直接指定入口函数为 mymain
+// 需要人肉处理 exit，否则会段错误
+// 所有代码都不依赖 libc
+
 void mymain()
 {
 	char msg[] = "hello world\n";
